50f122bfa5d0e99cb29e03a021e29c8528c5f759
Brightcells <brightcells@gmail.com>
2018-07-27 17:58:48 +0800

    :art: Return sessioninfo when not encryptedData

--- pywe_miniapp/miniapp.py
+++ pywe_miniapp/miniapp.py
@@ -28,16 +28,19 @@ class MiniApp(BaseWechat):
         # Update params
         self.update_params(appid=appid, secret=secret, storage=storage)
         # Fetch sessionInfo
-        session_info = self.get(self.JSCODE2SESSION, appid=self.appid, secret=self.secret, code=code, grant_type=grant_type)
+        session_info = self.get(self.JSCODE2SESSION, appid=self.appid, secret=self.secret, code=code, grant_type=grant_type) if code else {}
         # Store sessionKey
-        self.storage.set(self.sessionKey, session_info.get('session_key', ''))
+        if session_info:
+            self.storage.set(self.sessionKey, session_info.get('session_key', ''))
         return session_info
 
     def get_session_key(self, appid=None, secret=None, code=None, grant_type='authorization_code', storage=None):
         # Update params
         self.update_params(appid=appid, secret=secret, storage=storage)
         # Fetch sessionKey
+        # From storage
         session_key = '' if code else self.storage.get(self.sessionKey)
+        # From request api
         if not session_key:
             session_key = get_session_info(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, storage=self.storage).get('session_key', '')
         return session_key
@@ -45,6 +48,10 @@ class MiniApp(BaseWechat):
     def get_userinfo(self, appid=None, secret=None, code=None, grant_type='authorization_code', session_key=None, encryptedData=None, iv=None, storage=None):
         # Update params
         self.update_params(appid=appid, secret=secret, storage=storage)
+        # If not encryptedData return session_info
+        if not encryptedData:
+            return get_session_info(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, storage=self.storage)
+        # Update sessionKey
         if not session_key:
             session_key = self.get_session_key(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, storage=self.storage)
         return decrypt(appId=self.appid, sessionKey=session_key, encryptedData=encryptedData, iv=iv)
@@ -52,6 +59,10 @@ class MiniApp(BaseWechat):
     def get_phone_number(self, appid=None, secret=None, code=None, grant_type='authorization_code', session_key=None, encryptedData=None, iv=None, storage=None):
         # Update params
         self.update_params(appid=appid, secret=secret, storage=storage)
+        # If not encryptedData return session_info
+        if not encryptedData:
+            return get_session_info(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, storage=self.storage)
+        # Update sessionKey
         if not session_key:
             session_key = self.get_session_key(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, storage=self.storage)
         return decrypt(appId=self.appid, sessionKey=session_key, encryptedData=encryptedData, iv=iv)
