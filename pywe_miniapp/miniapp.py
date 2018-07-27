2f9f4356f64eafc1bc21f373aa4ee9dd5fd0cc0e
Brightcells <brightcells@gmail.com>
2018-07-27 17:40:59 +0800

    :art: Update params

--- pywe_miniapp/miniapp.py
+++ pywe_miniapp/miniapp.py
@@ -6,9 +6,10 @@ from pywe_storage import MemoryStorage
 
 
 class MiniApp(BaseWechat):
-    def __init__(self, appid=None, storage=None):
+    def __init__(self, appid=None, secret=None, storage=None):
         super(MiniApp, self).__init__()
         self.appid = appid
+        self.secret = secret
         self.storage = storage or MemoryStorage()
         # wx.login(OBJECT), Refer: https://mp.weixin.qq.com/debug/wxadoc/dev/api/api-login.html
         # wx.getUserInfo(OBJECT), Refer: https://mp.weixin.qq.com/debug/wxadoc/dev/api/open.html#wxgetuserinfoobject
@@ -18,34 +19,42 @@ class MiniApp(BaseWechat):
     def sessionKey(self):
         return '{0}:sessionKey'.format(self.appid)
 
-    def get_session_info(self, appid=None, secret=None, code=None, grant_type='authorization_code', storage=None):
-        # Fetch sessionInfo
-        session_info = self.get(self.JSCODE2SESSION, appid=appid or self.appid, secret=secret, code=code, grant_type=grant_type)
-        # Update params
+    def update_params(self, appid=None, secret=None, storage=None):
+        self.appid = appid or self.appid
+        self.secret = secret or self.secret
         self.storage = storage or self.storage
+
+    def get_session_info(self, appid=None, secret=None, code=None, grant_type='authorization_code', storage=None):
+        # Update params
+        self.update_params(appid=appid, secret=secret, storage=storage)
+        # Fetch sessionInfo
+        session_info = self.get(self.JSCODE2SESSION, appid=self.appid, secret=self.secret, code=code, grant_type=grant_type)
         # Store sessionKey
         self.storage.set(self.sessionKey, session_info.get('session_key', ''))
         return session_info
 
     def get_session_key(self, appid=None, secret=None, code=None, grant_type='authorization_code', storage=None):
         # Update params
-        self.appid = appid or self.appid
-        self.storage = storage or self.storage
+        self.update_params(appid=appid, secret=secret, storage=storage)
         # Fetch sessionKey
         session_key = '' if code else self.storage.get(self.sessionKey)
         if not session_key:
-            session_key = get_session_info(appid=appid, secret=secret, code=code, grant_type=grant_type, storage=storage).get('session_key', '')
+            session_key = get_session_info(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, storage=self.storage).get('session_key', '')
         return session_key
 
     def get_userinfo(self, appid=None, secret=None, code=None, grant_type='authorization_code', session_key=None, encryptedData=None, iv=None, storage=None):
+        # Update params
+        self.update_params(appid=appid, secret=secret, storage=storage)
         if not session_key:
-            session_key = self.get_session_key(appid=appid, secret=secret, code=code, grant_type=grant_type, storage=storage)
-        return decrypt(appId=appid, sessionKey=session_key, encryptedData=encryptedData, iv=iv)
+            session_key = self.get_session_key(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, storage=self.storage)
+        return decrypt(appId=self.appid, sessionKey=session_key, encryptedData=encryptedData, iv=iv)
 
     def get_phone_number(self, appid=None, secret=None, code=None, grant_type='authorization_code', session_key=None, encryptedData=None, iv=None, storage=None):
+        # Update params
+        self.update_params(appid=appid, secret=secret, storage=storage)
         if not session_key:
-            session_key = self.get_session_key(appid=appid, secret=secret, code=code, grant_type=grant_type, storage=storage)
-        return decrypt(appId=appid, sessionKey=session_key, encryptedData=encryptedData, iv=iv)
+            session_key = self.get_session_key(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, storage=self.storage)
+        return decrypt(appId=self.appid, sessionKey=session_key, encryptedData=encryptedData, iv=iv)
 
 
 miniapp = MiniApp()
