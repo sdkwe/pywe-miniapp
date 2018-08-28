05e8108b93ec937ae91273e81ca96e5ee16c9bfb
Brightcells <brightcells@gmail.com>
2018-08-28 16:18:05 +0800

    :bug: Fix Bug: sessionKey not public use

--- pywe_miniapp/miniapp.py
+++ pywe_miniapp/miniapp.py
@@ -15,60 +15,70 @@ class MiniApp(BaseWechat):
         # wx.getUserInfo(OBJECT), Refer: https://mp.weixin.qq.com/debug/wxadoc/dev/api/open.html#wxgetuserinfoobject
         self.JSCODE2SESSION = self.API_DOMAIN + '/sns/jscode2session?appid={appid}&secret={secret}&js_code={code}&grant_type={grant_type}'
 
-    @property
-    def sessionKey(self):
-        return '{0}:sessionKey'.format(self.appid)
+    def sessionKey(self, unid=None):
+        # https://developers.weixin.qq.com/community/develop/doc/00088a409fc308b765475fa4351000?highLine=session_key
+        # sessionKey 非共用
+        return '{0}:{1}:sessionKey'.format(unid or '', self.appid)
 
     def update_params(self, appid=None, secret=None, storage=None):
         self.appid = appid or self.appid
         self.secret = secret or self.secret
         self.storage = storage or self.storage
 
-    def get_session_info(self, appid=None, secret=None, code=None, grant_type='authorization_code', storage=None):
+    def store_session_key(self, appid=None, secret=None, session_key=None, unid=None, storage=None):
+        # Update params
+        self.update_params(appid=appid, secret=secret, storage=storage)
+        # Store sessionKey
+        if session_key and unid:
+            return self.storage.set(self.sessionKey(unid=unid), session_key)
+        return False
+
+    def get_session_info(self, appid=None, secret=None, code=None, grant_type='authorization_code', unid=None, storage=None):
         # Update params
         self.update_params(appid=appid, secret=secret, storage=storage)
         # Fetch sessionInfo
         session_info = self.get(self.JSCODE2SESSION, appid=self.appid, secret=self.secret, code=code, grant_type=grant_type) if code else {}
         # Store sessionKey
-        if session_info:
-            self.storage.set(self.sessionKey, session_info.get('session_key', ''))
+        if session_info and unid:
+            self.storage.set(self.sessionKey(unid=unid), session_info.get('session_key', ''))
         return session_info
 
-    def get_session_key(self, appid=None, secret=None, code=None, grant_type='authorization_code', storage=None):
+    def get_session_key(self, appid=None, secret=None, code=None, grant_type='authorization_code', unid=None, storage=None):
         # Update params
         self.update_params(appid=appid, secret=secret, storage=storage)
         # Fetch sessionKey
         # From storage
-        session_key = '' if code else self.storage.get(self.sessionKey)
+        session_key = '' if code or unid else self.storage.get(self.sessionKey(unid=unid))
         # From request api
         if not session_key:
             session_key = get_session_info(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, storage=self.storage).get('session_key', '')
         return session_key
 
-    def get_userinfo(self, appid=None, secret=None, code=None, grant_type='authorization_code', session_key=None, encryptedData=None, iv=None, storage=None):
+    def get_userinfo(self, appid=None, secret=None, code=None, grant_type='authorization_code', unid=None, session_key=None, encryptedData=None, iv=None, storage=None):
         # Update params
         self.update_params(appid=appid, secret=secret, storage=storage)
         # If not encryptedData return session_info
         if not encryptedData:
-            return get_session_info(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, storage=self.storage)
+            return get_session_info(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, unid=unid, storage=self.storage)
         # Update sessionKey
         if not session_key:
-            session_key = self.get_session_key(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, storage=self.storage)
+            session_key = self.get_session_key(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, unid=unid, storage=self.storage)
         return decrypt(appId=self.appid, sessionKey=session_key, encryptedData=encryptedData, iv=iv)
 
-    def get_phone_number(self, appid=None, secret=None, code=None, grant_type='authorization_code', session_key=None, encryptedData=None, iv=None, storage=None):
+    def get_phone_number(self, appid=None, secret=None, code=None, grant_type='authorization_code', unid=None, session_key=None, encryptedData=None, iv=None, storage=None):
         # Update params
         self.update_params(appid=appid, secret=secret, storage=storage)
         # If not encryptedData return session_info
         if not encryptedData:
-            return get_session_info(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, storage=self.storage)
+            return get_session_info(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, unid=unid, storage=self.storage)
         # Update sessionKey
         if not session_key:
-            session_key = self.get_session_key(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, storage=self.storage)
+            session_key = self.get_session_key(appid=self.appid, secret=self.secret, code=code, grant_type=grant_type, unid=unid, storage=self.storage)
         return decrypt(appId=self.appid, sessionKey=session_key, encryptedData=encryptedData, iv=iv)
 
 
 miniapp = MiniApp()
+store_session_key = miniapp.store_session_key
 get_session_info = miniapp.get_session_info
 get_session_key = miniapp.get_session_key
 get_userinfo = miniapp.get_userinfo
