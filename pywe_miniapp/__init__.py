4311b84c180768a3f2a9f297e0a3eac9cf8563ce
huangqimin001 <huangqimin001@ke.com>
2020-11-30 00:16:37 +0800

    :art: iSort & Pycodestyle

--- pywe_miniapp/__init__.py
+++ pywe_miniapp/__init__.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from pywe_miniapp.miniapp import (
-    store_session_key,
-    get_session_info,
-    get_session_key,
-    get_userinfo,
-    get_phone_number,
-    get_shareinfo,
-)
+from pywe_miniapp.miniapp import (get_phone_number, get_session_info, get_session_key, get_shareinfo, get_userinfo,
+                                  store_session_key)
